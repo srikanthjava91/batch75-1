9e16c0a46ab7b4e6a224265d16f717cdd7bd1cc2
Srikanth <srikanthjava91@gmail.com>
2026-06-22 13:00:22 +0530

    CRICKETER EXAMPLE

--- batch75corejavaproj1/src/com/javaintro/Test1.java
+++ batch75corejavaproj1/src/com/javaintro/Test1.java
@@ -2,8 +2,26 @@ package com.javaintro;
 
 public class Test1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int id;
+	String name;
+
+	static int orgId;
+	static String orgName;
+
+	{
+		System.out.println("Hello instance ");
+	}
+
+	// instance method
+	void main() {
+		System.out.println("main method started !");
+
+		int id = 10;
+
+		System.out.println(id);
+		System.out.println(name);
+		System.out.println(orgId);
+		System.out.println(orgName);
 
 	}
 
